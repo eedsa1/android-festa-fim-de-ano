29853153a7d48cc653dcde98dab3ab625500cf3d
Eric Alencar <eric_tonarede@hotmail.com>
2019-12-22 22:34:36 -0300

    implementação da exibição do dia atual e da lógica de exibir dias restantes

--- app/src/main/java/com/example/festafimdeano/view/DetailsActivity.java
+++ app/src/main/java/com/example/festafimdeano/view/DetailsActivity.java
@@ -1,23 +1,39 @@
 package com.example.festafimdeano.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
-
 import com.example.festafimdeano.R;
+import com.example.festafimdeano.constant.FimDeAnoConstants;
+import com.example.festafimdeano.data.SecurityPreferences;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder;
+    private SecurityPreferences mSecurityPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        this.mViewHolder.checkParticipate = (CheckBox) findViewById(R.id.check_participate);
+        this.mSecurityPreferences = new SecurityPreferences(this);
 
+        this.mViewHolder.checkParticipate = (CheckBox) findViewById(R.id.check_participate);
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.check_participate){
+           if(this.mViewHolder.checkParticipate.isChecked()){
+               this.mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE_KEY, FimDeAnoConstants.CONFIRMATION_YES);
+           }else{
+               this.mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE_KEY, FimDeAnoConstants.CONFIRMATION_NO);
+           }
+        }
     }
 
     private static class ViewHolder{
--- app/src/main/java/com/example/festafimdeano/view/MainActivity.java
+++ app/src/main/java/com/example/festafimdeano/view/MainActivity.java
@@ -9,21 +9,38 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.festafimdeano.R;
+import com.example.festafimdeano.constant.FimDeAnoConstants;
+import com.example.festafimdeano.data.SecurityPreferences;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder;
+    private SecurityPreferences mSecurityPreferences;
+    private static SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.mSecurityPreferences = new SecurityPreferences(this);
+
         this.mViewHolder.textToday = findViewById(R.id.text_today);
         this.mViewHolder.textDaysLeft = findViewById(R.id.text_days_left);
         this.mViewHolder.buttonConfirm = findViewById(R.id.button_confirm);
 
         this.mViewHolder.buttonConfirm.setOnClickListener(this);
+
+        //dia atual
+        this.mViewHolder.textToday.setText(SIMPLE_DATE_FORMAT.format(Calendar.getInstance().getTime()));
+        String daysLeft = String.format("%s %s", String.valueOf(this.getDaysLeft()), getString(R.string.dias));
+        this.mViewHolder.textDaysLeft.setText(daysLeft);
+
+        this.verifyPresence();
+
     }
 
     @Override
@@ -32,6 +49,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, DetailsActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void verifyPresence() {
+        String presence = this.mSecurityPreferences.getStoredString(FimDeAnoConstants.PRESENCE_KEY);
+        if(presence.equals("")){
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.nao_confirmado));
+        } else if(presence.equals(FimDeAnoConstants.CONFIRMATION_YES)){
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.sim));
+        } else{
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.nao));
+        }
+    }
+
+
+    private int getDaysLeft(){
+        //DATA ATUAL
+        Calendar calendarToday = Calendar.getInstance();
+        int today = calendarToday.get(Calendar.DAY_OF_YEAR);
+
+        //Dia máximo do ano
+        Calendar calendarLastDay = Calendar.getInstance();
+        int dayMax = calendarLastDay.getActualMaximum(Calendar.DAY_OF_YEAR);
+
+        return dayMax - today;
     }
 
     private static class ViewHolder{
